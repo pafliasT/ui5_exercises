ba590cdc265576d3eb5548d583f16561452da153
pafliasT <paflias13@gmail.com>
2024-01-16 13:51:13 +0000

    Call API

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,18 +1,26 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, JSONModel) {
         "use strict";
 
         return Controller.extend("project1.controller.View1", {
             onInit: function () {
 
             },
-            onPress: function () {
-                console.log('test');
+            onCallApiPress: function () {
+                var oModel = new JSONModel();
+                var sUrl = "https://jsonplaceholder.typicode.com/posts"; // API URL
+
+                oModel.loadData(sUrl);
+                oModel.attachRequestCompleted(function() {
+                    var data = oModel.getData();
+                    console.log(data); // Log the data
+                });
             }
         });
     });
